3c29ea727badc587312e4cce3954b59eecf22cdb
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-12 01:31:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 require("dotenv").config();
 
 module.exports = {
-  session_key: process.env.SESSION_KEY,
-  mongodb_url: process.env.MONGODB_URL || process.env.MONGO_URL || "",
-  telegramBotToken: process.env.BOT_TOKEN,
-  ownerID: process.env.OWNER_ID,
+  session_key: "Ibntaymya1.",
+  mongodb_url: "mongodb+srv://Bala7a19871:Ibntaymya1.@cluster0.5mtsc.mongodb.net/?retryWrites=true&w=majority",
+  telegramBotToken: "5871078140:AAEt8lSMgOxFtDWTtpOFbkcG-uyL8hYnRok",
+  ownerID: int(1227193881),
   pmReply: process.env.PM_REPLY,
   tgDownloadMax: 20971520, // 20 MB
   tgUploadMax: 52428800 // 50MB
